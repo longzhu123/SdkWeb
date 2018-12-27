5ca8c4515c4649b10fb26158445bf414956e0296
yangjie <1174716220@qq.com>
2018-12-27 15:57:43 +0800

    修改附件添加

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -172,12 +172,11 @@
                 <el-form-item label="附件" label-width="80px"  prop="aboutFile"  ref="addAboutFile">
                   <el-upload
                     action="https://jsonplaceholder.typicode.com/posts/"
-                    :on-success="aboutFileSuccess"
-                    :on-remove="aboutFileRemove"
+                    :on-success="addAboutFileSuccess"
+                    :on-remove="addAboutFileRemove"
                     :file-list="addForm.aboutFile"
                     multiple>
                     <el-button size="small" type="primary">点击上传</el-button>
-                    <el-button size="small" type="primary">下载</el-button>
                   </el-upload>
                 </el-form-item>
               </td>
@@ -225,20 +224,60 @@
           <table id="updateTable">
             <tr>
               <td>
-                <el-form-item label="ip" label-width="80px" prop="ip">
-                  <el-input v-model="updateForm.ip" placeholder="请输入ip"></el-input>
+                <el-form-item label="用户名" label-width="80px" prop="username">
+                  <el-input v-model="updateForm.username" placeholder="请输入用户名"></el-input>
                 </el-form-item>
               </td>
               <td>
-                <el-form-item label="操作内容" label-width="90px" prop="operContent">
-                  <el-input v-model="updateForm.operContent" placeholder="请输入操作内容"></el-input>
+                <el-form-item label="密码" label-width="80px" prop="password">
+                  <el-input v-model="updateForm.password" placeholder="请输入密码"></el-input>
+                </el-form-item>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <el-form-item label="年龄" label-width="80px"  prop="age">
+                  <el-input type="number" v-model.number="updateForm.age"  placeholder="请输入年龄"></el-input>
+                </el-form-item>
+              </td>
+              <td>
+                <el-form-item label="出生日期" label-width="80px"  prop="borth">
+                  <el-date-picker type="date" placeholder="选择出生日期" v-model="updateForm.borth" style="width: 100%"></el-date-picker>
                 </el-form-item>
               </td>
             </tr>
             <tr>
               <td colspan="2">
-                <el-form-item label="操作耗时" label-width="80px"  prop="taskTimeSpan">
-                  <el-input type="number" v-model.number="updateForm.taskTimeSpan"  placeholder="请输入操作耗时"></el-input>
+                <el-form-item label="学历" label-width="80px"  prop="edu">
+                  <el-select v-model="updateForm.edu" filterable placeholder="请选择学历" style="width: 100%">
+                    <el-option
+                      v-for="item in eduList"
+                      :key="item.value"
+                      :label="item.label"
+                      :value="item.value">
+                    </el-option>
+                  </el-select>
+                </el-form-item>
+              </td>
+            </tr>
+            <tr>
+              <td colspan="2">
+                <el-form-item label="附件" label-width="80px"  prop="aboutFile"  ref="updateAboutFile">
+                  <el-upload
+                    action="https://jsonplaceholder.typicode.com/posts/"
+                    :on-success="updateAboutFileSuccess"
+                    :on-remove="updateAboutFileRemove"
+                    :file-list="updateForm.aboutFile"
+                    multiple>
+                    <el-button size="small" type="primary">点击上传</el-button>
+                  </el-upload>
+                </el-form-item>
+              </td>
+            </tr>
+            <tr>
+              <td colspan="2">
+                <el-form-item label="备注" label-width="80px"  prop="comment">
+                  <el-input type="textarea" :autosize="{minRows:5}" v-model="updateForm.comment"  placeholder="请输入备注"></el-input>
                 </el-form-item>
               </td>
             </tr>
@@ -449,14 +488,23 @@
           alert("网络异常");
         });
       },
-      //附件上传成功callback
-      aboutFileSuccess(res, file){
+      //添加附件对象上传成功callback
+      addAboutFileSuccess(res, file){
         this.addForm.aboutFile.push(file);
         this.$refs.addAboutFile.clearValidate();
       },
-      //删除附件callback  file:当前删除的附件对象  fileList:删除后剩余的附件集合
-      aboutFileRemove(file, fileList){
+      //添加附件对象删除成功callback   file:当前删除的附件对象  fileList:删除后剩余的附件集合
+      addAboutFileRemove(file, fileList){
         this.addForm.aboutFile=fileList;
+      },
+      //修改附件对象上传成功callback
+      updateAboutFileSuccess(res, file){
+        this.updateForm.aboutFile.push(file);
+        this.$refs.updateAboutFile.clearValidate();
+      },
+      //修改附件对象删除成功callback  file:当前删除的附件对象  fileList:删除后剩余的附件集合
+      updateAboutFileRemove(file, fileList){
+        this.updateForm.aboutFile=fileList;
       },
       //编辑click事件
       handleEdit() {
